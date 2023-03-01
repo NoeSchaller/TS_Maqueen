dd2e2e155e406b8155d8a4e48175d4016d1f7359
NoeSchaller <tamialunaire@gmail.com>
2023-03-01 14:57:01 +0000

    ajout du dossier build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
     },
     resolve: {
         extensions: [".ts"],

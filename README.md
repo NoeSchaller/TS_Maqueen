e1c7dac72d3a18ed08bbb83e69e261742e602195
Noé Schaller <tamialunaire@gmail.com>
2023-04-21 17:45:10 +0200

    Final si j'ai pas plus de temps

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-cloned from https://github.com/geocine/phaser3-rollup-typescript
+Modificiation autre que le typeScript et Vite:
+- Les robots se déplacent beaucoup plus droit
+- Corrections de bugs de collision avec les zones
+- Corrections du paramètre angle des 2 robots
+
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { Simulation } from "./simulation/simulation";
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+/*
 
 function load(scene: any) {
   scene.load.image("yes", "/itWork.jpg");
@@ -51,4 +52,4 @@ async function main() {
   console.log(sim.robots[0].angle)
 }
 
-main();
+main();*/
